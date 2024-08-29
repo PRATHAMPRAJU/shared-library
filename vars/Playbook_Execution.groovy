92f935ad5e35d0d184d32b37103e27a8d5459c08
Pratham Kukudkar <87323200+PRATHAMPRAJU@users.noreply.github.com>
2024-08-29 21:29:01 +0530

    Update Playbook_Execution.groovy

--- vars/Playbook_Execution.groovy
+++ vars/Playbook_Execution.groovy
@@ -1,8 +1,8 @@
 def call() {
     stage('playbook execution') {
         sh '''
-        ansible-playbook -i /home/ubuntu/Docker_role/Docker/tests/inventory /home/ubuntu/Docker_role/Docker/tests/test.yml \
-        --ssh-extra-args="-o StrictHostKeyChecking=no"
+        ansible-playbook -i /home/ubuntu/Docker_role/Docker/tests/inventory /home/ubuntu/Docker_role/Docker/tests/test.yml 
+        
         '''
     }
 }
