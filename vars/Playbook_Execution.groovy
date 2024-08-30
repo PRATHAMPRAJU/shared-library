b74fe74f0ebdd791031172ede1473219d0290218
Pratham Kukudkar <87323200+PRATHAMPRAJU@users.noreply.github.com>
2024-08-31 00:42:38 +0530

    Update Playbook_Execution.groovy

--- vars/Playbook_Execution.groovy
+++ vars/Playbook_Execution.groovy
@@ -1,8 +1,8 @@
 def call() {
-    stage('playbook execution') {
-        sh '''
-        ansible-playbook -i /home/ubuntu/Docker_role/Docker/tests/inventory /home/ubuntu/Docker_role/Docker/tests/test.yml 
-        
-        '''
+    stage('Playbook Execution') {
+        // Set the PATH variable to include the location of ansible-playbook
+        withEnv(["PATH+AN=/opt/homebrew/bin"]) {
+            sh "ansible-playbook -i /home/ubuntu/Docker_role/Docker/tests/inventory /home/ubuntu/Docker_role/Docker/tests/test.yml"
+        }
     }
 }
